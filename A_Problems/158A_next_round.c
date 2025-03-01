773cd6f8386a39b4c032ee46e44949f7084714c0
Fabian0594 <142474336+Fabian0594@users.noreply.github.com>
2025-03-01 22:10:43 +0000

    158A In progress

--- A_Problems/158A_next_round.c
+++ A_Problems/158A_next_round.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int maint() {
-    
-    
+int compare(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
+}
+
+int main() {
+    int n, k;
+    scanf("%d %d", &n, &k);
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    qsort(arr, n, sizeof(int), compare);
+    // for (int i = 0; i < n; i++) {
+    //     printf("%d ", arr[i]);
+    // }
     
     return 0;
 }
