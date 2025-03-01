1257a9121b4e23b26d7e2d74c383fc5b7e95e98e
Fabian0594 <142474336+Fabian0594@users.noreply.github.com>
2025-03-01 18:30:16 +0000

    Watermelon done

---
+++ A_Problems/A4_Watermelon.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    int watermelon_weigh = 0;
+    scanf("%i", &watermelon_weigh);
+    if (watermelon_weigh % 2 == 0)
+        printf("YES\n");
+    else 
+        printf("No\n");
+
+
+}
