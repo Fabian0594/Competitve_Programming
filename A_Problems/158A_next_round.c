7fbd85e6ec167b8fd8a00f104966365ed075b9e4
Fabian0594 <142474336+Fabian0594@users.noreply.github.com>
2025-03-03 12:48:03 +0000

    #

--- A_Problems/158A_next_round.c
+++ A_Problems/158A_next_round.c
@@ -5,6 +5,15 @@ int compare(const void *a, const void *b) {
     return (*(int *)a - *(int *)b);
 }
 
+int lineal_search(int arr[], int element, int arr_size) {
+    for (int i = 0; i < arr_size; i++)
+    {
+        if(arr[i] == element)
+            return element;
+    }
+    return -1;
+}
+
 int main() {
     int n, k;
     scanf("%d %d", &n, &k);
@@ -13,9 +22,11 @@ int main() {
         scanf("%d", &arr[i]);
     }
     qsort(arr, n, sizeof(int), compare);
-    // for (int i = 0; i < n; i++) {
-    //     printf("%d ", arr[i]);
-    // }
-    
+    int first_ocurrence = lineal_search(arr, k, n);
+    if (first_ocurrence == -1)
+    {
+        printf("");
+    } 
+
     return 0;
 }
